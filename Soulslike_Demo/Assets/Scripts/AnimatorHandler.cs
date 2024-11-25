507a97d16b474402ac6036e44c94de1cecfd7548
Nathan19971118 <ccw3674@gmail.com>
2024-11-25 14:55:49 +0900

    Player Movement Complete

---
+++ Soulslike_Demo/Assets/Scripts/AnimatorHandler.cs
@@ -0,0 +1,104 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorHandler : MonoBehaviour
+{
+    public Animator anim;
+    float horizontal;
+    float vertical;
+    public bool canRotate;
+
+    private void Start()
+    {
+        Initialize();
+        CanRotate();
+    }
+
+    void Initialize()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdateAnimatorValues(float verticalMovement, float horizontalMovement)
+    {
+        #region Vertical
+        float v = 0;
+
+        if (verticalMovement > 0 && verticalMovement < 0.55f)
+        {
+            v = 0.55f;
+        }
+        else if (verticalMovement > 0.55f)
+        {
+            v = 1;
+        }
+        else if (verticalMovement < 0 && verticalMovement > -0.55f)
+        {
+            v = -0.5f;
+        }
+        else if (verticalMovement < -0.55f)
+        {
+            v = -1;
+        }
+        else
+        {
+            v = 0;
+        }
+        #endregion
+
+        #region Horizontal
+        float h = 0;
+
+        if (horizontalMovement > 0 && horizontalMovement < 0.55f)
+        {
+            h = 0.55f;
+        }
+        else if (horizontalMovement > 0.55f)
+        {
+            h = 1;
+        }
+        else if (horizontalMovement < 0 && horizontalMovement > -0.55f)
+        {
+            h = -0.5f;
+        }
+        else if (horizontalMovement < -0.55f)
+        {
+            h = -1;
+        }
+        else
+        {
+            h = 0;
+        }
+        #endregion
+
+        anim.SetFloat("Vertical", v, 0.1f, Time.deltaTime);
+        anim.SetFloat("Horizontal", h, 0.1f, Time.deltaTime);
+    }
+
+    public void HandleJumpAnim()
+    {
+        anim.SetTrigger("isJump");
+    }
+
+    public void HandleDodgeAnim()
+    {
+        anim.SetTrigger("isDodge");
+    }
+
+    public void CanRotate()
+    {
+        canRotate = true;
+    }
+
+    public void StopRotation()
+    {
+        canRotate = false;
+    }
+}
---
+++ Soulslike_Demo/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,160 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem.Processors;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Movement")]
+    public float moveSpeed;
+    public float rotationSpeed = 7f;
+    public float jumpForce;
+
+    [Header("Ground Check")]
+    public bool isGrounded;
+
+    [Header("Dodge")]
+    public float dodgeForce;
+    public float dodgeCooldown = 0.5f;
+    bool isDodge = false;
+
+    [Header("References")]
+    public Transform orientation;
+
+    [Header("Input")]
+    float horizontalInput;
+    float verticalInput;
+    bool jumpInput;
+    bool dodgeInput;
+
+    [HideInInspector]
+    public AnimatorHandler animatorHandler;
+    public Rigidbody rigidBody;
+    float dodgeTimer;
+
+
+    private void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        animatorHandler=GetComponentInChildren<AnimatorHandler>();
+        rigidBody.freezeRotation = true;
+        isGrounded = true;
+    }
+
+    private void Update()
+    {
+        GetUserInput();
+
+        if (jumpInput && isGrounded)
+        {
+            Jump();
+        }
+
+        if (dodgeInput && dodgeTimer <= 0)
+        {
+            Dodge();
+        }
+
+        if (dodgeTimer > 0)
+        {
+            dodgeTimer -= Time.deltaTime;
+        } 
+    }
+
+    private void FixedUpdate()
+    {
+        if (!isDodge)
+        {
+            HandleMovementAndRotation();
+        }
+    }
+
+    private void GetUserInput()
+    {
+        // Get Input Axis
+        horizontalInput = Input.GetAxis("MoveHorizontal");
+        verticalInput = Input.GetAxis("MoveVertical");
+
+        // Get jump Input
+        jumpInput = Input.GetButtonDown("Jump");
+
+        // Get dodge Input
+        dodgeInput = Input.GetButtonDown("Dodge");
+
+        // Update animator values
+        animatorHandler.UpdateAnimatorValues(verticalInput, horizontalInput);
+
+    }
+
+    private void HandleMovementAndRotation()
+    {
+        if (animatorHandler.canRotate)
+        {
+            // Only process if there's input
+            if (horizontalInput != 0 || verticalInput != 0)
+            {
+                // Calculate the input direction
+                Vector3 inputDirection = new Vector3(horizontalInput, 0, verticalInput);
+                float inputMagnitude = inputDirection.magnitude;
+
+                // Normalize the direction and handle deadzone
+                if (inputMagnitude > 0.19f) // Small deadzone to prevent drift
+                {
+                    // Calculate target rotation from input
+                    float targetAngle = Mathf.Atan2(horizontalInput, verticalInput) * Mathf.Rad2Deg;
+                    Quaternion targetRotation = Quaternion.Euler(0, targetAngle, 0);
+
+                    // Smoothly rotate to fack movement direction
+                    transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+
+                    // Move forward in the direction we're facing
+                    transform.position += transform.forward * moveSpeed * inputMagnitude * Time.deltaTime;
+                }
+            }
+        }
+    }
+
+    private void Jump()
+    {
+        if (jumpInput && isGrounded && !isDodge)
+        {
+            rigidBody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            animatorHandler.HandleJumpAnim();
+            isGrounded = false;
+        }
+    }
+
+    private void Dodge()
+    {
+        if (dodgeInput && !isDodge)
+        {
+            Vector3 dodgeDir = new Vector3(horizontalInput, 0, verticalInput).normalized;
+
+            if (dodgeDir.magnitude == 0)
+            {
+                dodgeDir = transform.forward;
+            }
+            rigidBody.AddForce(dodgeDir * dodgeForce, ForceMode.Impulse);
+            animatorHandler.HandleDodgeAnim();
+            isDodge = true;
+        }
+
+        dodgeTimer = dodgeCooldown;
+
+        // Reset isDodge after a short delay
+        Invoke(nameof(ResetDodge), dodgeCooldown);
+    }
+
+    private void ResetDodge()
+    {
+        isDodge = false;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+}
